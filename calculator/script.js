46c7ac7ae78f350d97b5717c05209637d0700088
prabin738 <prabinsingh750@gmail.com>
2025-07-31 12:57:10 +0545

    analog cal html and css done

---
+++ calculator/script.js
@@ -0,0 +1,10 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let srting = "";
+let arr = Array.from(buttons);
+arr.forEach(button =>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML)
+    })
+})
